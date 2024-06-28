99911f1500aa568f36b14e0ca88df77211888d71
axb157030 <amine.benelbar.1@gmail.com>
2024-06-28 12:23:49 -0500

    Create ElasticIPSample1.tf

---
+++ Elastic IP/ElasticIPSample1.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_eip" "loadBalancer" {
+  domain = "vpc"
+}
